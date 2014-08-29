b1b46cf5e5c2b0c491151988a4b5dc4cee55df4c
Anand Patil <anand.hp@gmail.com>
2014-08-29 11:01:33 +0530

    Inorder interative tree traversal

--- BinaryTreeInorderTraversal.cpp
+++ BinaryTreeInorderTraversal.cpp
@@ -25,9 +25,48 @@ public:
         inorder(root->right, path);
     }
     
+    void inorderTraversalIterative(TreeNode* root, vector<int>& path) {
+        if (!root)
+            return;
+        stack<TreeNode*> s;
+        s.push(root);
+        
+        TreeNode *prev = NULL, *current = NULL;
+        
+        while (!s.empty()) {
+            current = s.top();
+            if(!prev || prev->left == current || prev->right == current) {
+                if (current->left) {
+                    s.push(current->left);
+                }
+                else if (current->right) {
+                    path.push_back(current->val);
+                    if (current->right)
+                        s.push(current->right);
+                }
+                else {
+                    path.push_back(current->val);
+                    s.pop();
+                }
+            }
+            else if (current->left == prev) {
+                path.push_back(current->val);
+                if (current->right)
+                    s.push(current->right);
+                else
+                    s.pop();
+            }
+            else if (current->right == prev){
+                s.pop();
+            }
+            prev = current;
+        }
+        return;
+    }    
     vector<int> inorderTraversal(TreeNode *root) {
         vector<int> inorderPath;
-        inorder(root, inorderPath);
+        //inorder(root, inorderPath);
+        inorderTraversalIterative(root, inorderPath);
         return inorderPath;
     }
 };
